952a18183247bd9e0c3da2ead873268dee969c0a
Abdirashidnoor <abdirashid16112@gmail.com>
2023-04-25 09:54:48 +0300

    completed

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -5,3 +5,26 @@ function Comment(props) {
 }
 
 export default Comment;
+
+
+function BlogPost() {
+  return (
+    <div>
+      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+      <Comment commentText="I agree with this statement. - Angela Merkel" />
+      <Comment commentText="A universal truth. - Noam Chomsky" />
+      <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+    </div>
+  );
+}
+
+
+<div>
+  <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+
+  <div>I agree with this statement. - Angela Merkel</div>
+
+  <div>A universal truth. - Noam Chomsky</div>
+
+  <div>Truth is singular. Its ‘versions’ are mistruths - Sonmi-451</div>
+</div>
